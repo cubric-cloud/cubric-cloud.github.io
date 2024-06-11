7dbedd5313f5f105150ed36375bde216ea2d726e
cubric-cloud <cubric.pln@gmail.com>
2024-06-11 10:56:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,26 +14,38 @@ mobileMenuModal.addEventListener('click', (event) => {
 
 // 배경 이미지 페이드 인 애니메이션
 const bgImage = document.querySelector('.bg-image');
-const mainText = document.querySelector('.main-content h1');
+const mainText = document.querySelector('.coming-soon');
 const hiddenText = document.querySelector('.hidden-text');
 
 window.addEventListener('load', () => {
   bgImage.classList.add('fade-in');
   setTimeout(() => {
     mainText.classList.add('fade-in');
+    setTimeout(() => {
+      hiddenText.classList.add('visible');
+    }, 2000); // 'Coming soon!' 애니메이션이 끝난 후 'AI for a Better Life, with Cubric'가 나타남
   }, 1000);
+});
 
-  mainText.addEventListener('animationend', () => {
-    hiddenText.classList.add('visible');
+// 메뉴 클릭 시 준비중입니다 팝업
+const menuLinks = document.querySelectorAll('nav ul li a, .mobile-menu-modal nav ul li a');
+const popup = document.getElementById('popup');
+const closeBtn = document.querySelector('.close-btn');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    popup.style.display = 'flex';
   });
 });
 
-// 메뉴 클릭시 준비중입니다 알림
-const navLinks = document.querySelectorAll('nav ul li a, .mobile-menu-modal nav ul li a');
-
-navLinks.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('준비중입니다.');
-  });
+closeBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
 });
+
+window.addEventListener('click', (event) => {
+  if (event.target === popup) {
+    popup.style.display = 'none';
+  }
+});
+
